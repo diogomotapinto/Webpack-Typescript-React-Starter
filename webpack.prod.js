63ac0582f913a53881269c5f18237116865abe91
Diogo Mota Pinto <up201404527@fe.up.pt>
2020-08-20 17:18:23 +0100

    add to heroku

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,6 +7,9 @@ module.exports = merge(common, {
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    contentBase: "./build",
   },
 });
